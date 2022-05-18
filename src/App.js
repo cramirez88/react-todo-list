0f8b8df5a9575f8052104324760cdd373ffec1f9
Christian Ramirez <christian.r0602@gmail.com>
2022-05-18 13:19:55 -0500

    Displaying todos on screen

--- src/App.js
+++ src/App.js
@@ -7,14 +7,37 @@ function App() {
   // second state takes care of the list that will get rendered everytime a new todo is added. Stored in an array because there will be more than one
   const [todos, setTodos] = useState([])
 
+  const handleClickTodo = e => {
+    // To avoid adding empty strings into array:
+    if (todo === ''){
+      return
+    }
+    e.preventDefault()
+    setTodos([...todos, todo])
+    // Calling setTodo will clear the state, but due to the one way flow of data, it will only clear the state. We have to connect the value of the input to the state
+    setTodo('')
+
+  }
+
   return (
     <div>
       {/* In the input, the user will enter a todo item. The todo will constantly chnage, so we have to keep track of state and resetting it */}
-      <input type="text" onChange={e => {setTodo(e.target.value)}}></input>
-      <button>Add Todo</button>
+      <input type="text" onChange={e => {setTodo(e.target.value)}} value={todo}></input>
+      {/* When the user adds a todo (by clicking on button), the todo will get saved to the array, added to screen, and clear input field */}
+      <button onClick={handleClickTodo}>Add Todo</button>
+      {
+        todos.map((todo, index) => {
+          return (
+            <div key={index}>{todo}</div>
+          )
+        })
+      }
     </div>
+      
   )
+  // I want to go through the array of my todos and display them on the screen
 
+  
 
 }
 
