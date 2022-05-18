ff375ec318917adb5d37fcdd2ebdaba77d9a91f1
Christian Ramirez <christian.r0602@gmail.com>
2022-05-18 12:44:18 -0500

    Added state to the todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import React, {useState} from 'react'
 
 function App() {
+  // We will need state to keep track of changes in input and list of todos 
+  // first state takes care of the input changes
+  const [todo, setTodo] = useState('') 
+  // second state takes care of the list that will get rendered everytime a new todo is added. Stored in an array because there will be more than one
+  const [todos, setTodos] = useState([])
+
   return (
-    <h1>Todo List</h1>
-  );
+    <div>
+      {/* In the input, the user will enter a todo item. The todo will constantly chnage, so we have to keep track of state and resetting it */}
+      <input type="text" onChange={e => {setTodo(e.target.value)}}></input>
+      <button>Add Todo</button>
+    </div>
+  )
+
+
 }
 
 export default App;
