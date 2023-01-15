df66e7f7ce8bd3e16bcf9feaeb3eefc15ce2d873
Christian Ramirez <christian.r0602@gmail.com>
2023-01-14 19:53:15 -0600

    toggling todos state

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,8 +4,13 @@ export default function Todos(props) {
   const {todos, setTodos} = props
 
 
-  const toggleComplete = () => {
-   
+  const toggleComplete = (todo) => {
+   if(todo.completed === false){
+    todo.completed = true
+   }else {
+    todo.completed = false
+   }
+   setTodos([...todos])
   }
 
   const deleteTodo = (id) => {
@@ -22,7 +27,7 @@ export default function Todos(props) {
             
             <div key={index}>
             <li style={{display: 'inline'}} >{todo.task}</li>
-            <input  style={{display: 'inline'}} type={'checkbox'}></input>
+            <input  style={{display: 'inline'}} type={'checkbox'} onClick={() => toggleComplete(todo)}></input>
             <button style={{display: 'inline'}} onClick={e => deleteTodo(todo.id)} >Delete</button>
             </div>
             
